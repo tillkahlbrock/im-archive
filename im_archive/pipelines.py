09f8c4af20ba209235f524ada040dde17da12f25
Till Kahlbrock <till@jimdo.com>
2015-02-16 08:04:37 +0100

    saves episode to cloudant

--- im_archive/pipelines.py
+++ im_archive/pipelines.py
@@ -1,4 +1,19 @@
 # -*- coding: utf-8 -*-
+import cloudant
+import uuid
+import os
+
 class ImArchivePipeline(object):
     def process_item(self, item, spider):
+        USERNAME = os.environ['CLOUDANT_USERNAME']
+        PASSWORD = os.environ['CLOUDANT_PASSWORD']
+        account = cloudant.Account('tillkahlbrock')
+
+        login = account.login(USERNAME, PASSWORD)
+        assert login.status_code == 200
+
+        db = account.database('ima')
+        doc = db.document('test_doc')
+        id = str(uuid.uuid4())
+        db[id] = { '_id': 'hello_world', 'name': item['name'], 'file': item['file'] }
         return item
