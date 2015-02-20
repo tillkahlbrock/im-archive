50e6becfc8677007c63b1ee35c1c5fc4fcc4dea2
Till Kahlbrock <till@jimdo.com>
2015-02-20 08:33:47 +0100

    clean it a bit

--- im_archive/pipelines.py
+++ im_archive/pipelines.py
@@ -7,13 +7,14 @@ class ImArchivePipeline(object):
     def process_item(self, item, spider):
         USERNAME = os.environ['CLOUDANT_USERNAME']
         PASSWORD = os.environ['CLOUDANT_PASSWORD']
-        account = cloudant.Account('tillkahlbrock')
+        ACCOUNT = os.environ['CLOUDANT_ACCOUNT']
+
+        account = cloudant.Account(os.environ['ACCOUNT')
 
         login = account.login(USERNAME, PASSWORD)
         assert login.status_code == 200
 
         db = account.database('ima')
-        doc = db.document('test_doc')
         id = str(uuid.uuid4())
-        db[id] = { '_id': 'hello_world', 'name': item['name'], 'file': item['file'] }
+        db[id] = { '_id': id, 'name': item['name'], 'file': item['file'] }
         return item
